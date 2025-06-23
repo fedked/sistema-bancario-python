1d5ed9be2527f976627f35ef43c462ecc2a8d37f
Fernando Kendi <fedked@outlook.com>
2025-06-22 23:26:28 -0300

    adiciona timestamp ao historico com datetimeC:/Users/fedke/AppData/Local/Programs/Python/Python313/python.exe c:/Users/fedke/workspace/python-bank-system/src/conta-bancaria.py

--- src/conta-bancaria.py
+++ src/conta-bancaria.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 
 # Constante Global
 TAXA_SAQUE = 2.50
@@ -9,18 +10,20 @@ class ContaBancaria:
     
     def depositar(self, valor):  
         self.saldo += valor
+        self.historico.append(f"{datetime.now().strftime('Data: %d/%m/%Y - Hora: %H:%M')} | Depósito: +R${valor:.2f}")
 
     def sacar(self, valor):
         valor_total = valor + TAXA_SAQUE    # Taxa fixa de R$ 2.50 
         if self.saldo >= valor_total:       # Se tiver dinheiro
             self.saldo -= valor_total       # Subtrai do saldo
+            self.historico.append(f"{datetime.now().strftime('Data: %d/%m/%Y - Hora: %H:%M')} | Saque -R${valor:.2f} (taxa: R${TAXA_SAQUE:.2f})")
             return True                     # Saque realizado
         else:
             print("Saldo insuficiente!")    # Aviso
             return False                    # Saque negado
     
     def extrato(self):
-        return f"Titular: {self.titular} | Saldo: R${self.saldo:.2f}"
+        return f"\nTitular: {self.titular} | Saldo: R${self.saldo:.2f}"
     
     def transferir(self, conta_destino, valor):
         taxa = 1.50 # Taxa fixa por transferência
@@ -29,16 +32,16 @@ class ContaBancaria:
         if self.saldo >= valor_total:
             self.saldo -= valor_total
             conta_destino.depositar(valor) # Deposita apenas o valor
-            
-            self.historico.append(f"Transferência enviada: -R${valor:.2f} (Taxa: R${taxa:.2f})")
-            conta_destino.historico.append(f"Transferência recebida: +R${valor:.2f}")
+            timestime = datetime.now().strftime('%d/%m/%Y %H:%M')
+            self.historico.append(f"{timestime} | Transferência enviada: -R${valor:.2f} (Taxa: R${taxa:.2f})")
+            conta_destino.historico.append(f" {timestime} | Transferência recebida: +R${valor:.2f}")
             return True
         else:
             print(f"Saldo insuficiente! Necessário: R${valor_total:.2f}")
             return False
         
     def ver_historico(self):
-        print("\Histórico de Operações:")
+        print("\nHistórico de Operações:")
         for operacao in self.historico:
             print(operacao)
 
@@ -47,9 +50,11 @@ def criar_conta(nome, saldo_inicial=0):
 
 conta = criar_conta("Fernando", 100) # Usando a função criadora
 conta.sacar(20)
+conta.ver_historico()
 print(conta.extrato())
+print("------------")
 
 conta2 = criar_conta("Fulano", 50)
-conta.transferir(conta2, 30)
-conta.ver_historico()
+conta2.sacar(5)
+conta2.ver_historico()
 print(conta.extrato())
